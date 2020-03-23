d6e9432626ffb27e0e6e60377ba18aa3206f64d0
Mahmoud Elgazzar <mahmoudgamalfcis55@gmail.com>
2020-03-23 13:53:49 +0200

    Update get_imgs_test_path.py
    we modified the path of input file which contains the paths of testing images

--- get_imgs_test_path.py
+++ get_imgs_test_path.py
@@ -3,7 +3,7 @@
 import os
 import glob
 
-train_images_path = "/home/chenxp/data/genome/im2p_test"
+train_images_path = "/content/im2p/data/im2p_test"
 train_images = glob.glob(train_images_path + "/*.jpg")
 
 f = open("imgs_test_path.txt", "w")
