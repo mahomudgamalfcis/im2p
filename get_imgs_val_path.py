77125ab6445c45a633939d1727be578ff6239c51
Mahmoud Elgazzar <mahmoudgamalfcis55@gmail.com>
2020-03-23 13:57:10 +0200

    Update get_imgs_val_path.py
    we modified the path of input file which contains the paths of validation images

--- get_imgs_val_path.py
+++ get_imgs_val_path.py
@@ -3,7 +3,7 @@
 import os
 import glob
 
-train_images_path = "/home/chenxp/data/genome/im2p_val"
+train_images_path = "/content/im2p/data/im2p_val"
 train_images = glob.glob(train_images_path + "/*.jpg")
 
 f = open("imgs_val_path.txt", "w")
